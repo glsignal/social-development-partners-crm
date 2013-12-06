f3020dd10b9f4394cc54c055e307d2efb12d6085
Zygar <a@andywilkinson.co.nz>
2013-12-07 11:27:48 +1300

    hairy initial commit

---
+++ app/js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'firebase',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'itemsCtrl'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+.controller('itemsCtrl', ["$scope", "$rootScope", "angularFireCollection", function ($scope, $rootScope, angularFireCollection) {
+    var ref = new Firebase("https://sdp-cms.firebaseio.com/");
+    $scope.items = angularFireCollection(ref, function(i){
+        console.log(i.val());
+    });
+}]);
+
