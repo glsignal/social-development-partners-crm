b4c9d5d7a4349b16553cec71149cd590a954050d
Zygar <a@andywilkinson.co.nz>
2013-12-07 12:00:05 +1300

    Adds a view for individual contact.

--- app/js/app.js
+++ app/js/app.js
@@ -11,14 +11,8 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'itemsCtrl'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'listMembers'});
+  $routeProvider.when('/view2/:id', {templateUrl: 'partials/contact.html', controller: 'singleContact'});
   $routeProvider.otherwise({redirectTo: '/view1'});
-}])
-.controller('itemsCtrl', ["$scope", "$rootScope", "angularFireCollection", function ($scope, $rootScope, angularFireCollection) {
-    var ref = new Firebase("https://sdp-cms.firebaseio.com/organisations");
-    $scope.items = angularFireCollection(ref, function(i){
-        console.log(i.val());
-    });
 }]);
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
+  controller('listMembers', ["$scope", "$rootScope", "angularFireCollection", function ($scope, $rootScope, angularFireCollection) {
+    var ref = new Firebase("https://sdp-cms.firebaseio.com/organisations");
+    $scope.items = angularFireCollection(ref, function(i){
+        console.log(i.val());
+    });
+}])
+  .controller('singleContact', ["$scope", "$rootScope", "angularFire", "$routeParams", function ($scope, $rootScope, angularFire, $routeParams) {
+    var contactId = $routeParams.id;
+    var ref = new Firebase("https://sdp-cms.firebaseio.com/contacts/" + contactId);
+    angularFire(ref, $scope, "contact");
   }]);
