0e3b906ea7996dd8589a5d7cb8f2ca392d562f99
Zygar <a@andywilkinson.co.nz>
2013-12-07 13:33:17 +1300

    this changes some stuff

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,14 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('listMembers', ["$scope", "$rootScope", "angularFireCollection", function ($scope, $rootScope, angularFireCollection) {
-    var ref = new Firebase("https://sdp-cms.firebaseio.com/organisations");
-    $scope.items = angularFireCollection(ref, function(i){
-        console.log(i.val());
+controller('listMembers', ["$scope", "$rootScope", "angularFireCollection",
+  function($scope, $rootScope, angularFireCollection) {
+    var orgs = new Firebase("https://sdp-cms.firebaseio.com/organisations");
+
+    $scope.organisations = angularFireCollection(orgs, function(i) {
+      console.log(i.val());
     });
-}])
-  .controller('singleContact', ["$scope", "$rootScope", "angularFire", "$routeParams", function ($scope, $rootScope, angularFire, $routeParams) {
-    var contactId = $routeParams.id;
-    var ref = new Firebase("https://sdp-cms.firebaseio.com/contacts/" + contactId);
-    angularFire(ref, $scope, "contact");
-  }]);
+
+  }
+])
+  .controller('singleContact', ["$scope", "$rootScope", "angularFire", "$routeParams",
+    function($scope, $rootScope, angularFire, $routeParams) {
+      var contactId = $routeParams.id;
+      var ref = new Firebase("https://sdp-cms.firebaseio.com/contacts/" + contactId);
+      angularFire(ref, $scope, "contact");
+    }
+  ]);
